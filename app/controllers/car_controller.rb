d234b6e7c10e8e296ff05f4fc128ff9e6e92be4a
Vitor Rocha <vitor.rocha81@bol.com.br>
2015-01-08 20:37:52 -0200

    adicionado  tags model

--- app/controllers/car_controller.rb
+++ app/controllers/car_controller.rb
@@ -1,4 +1,10 @@
 class CarController < ApplicationController
   def details
+  	@modelo = "Modelo"
+  	@marca = "Marca"
+  	@valor = "Preço"
+  	@ano = "Ano"
+  	@km = "KM"
+  	@motor = "Motor"
   end
 end
