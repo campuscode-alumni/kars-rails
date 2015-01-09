f0b14ed6a3af6b604bfba026507562ddcf7f9dd6
Andre <andre.marques212747@hotmail.com>
2015-01-09 19:02:26 -0200

    criando a View e a controller

--- app/controllers/car_controller.rb
+++ app/controllers/car_controller.rb
@@ -1,4 +1,10 @@
 class CarController < ApplicationController
   def details
   end
+
+  def cars
+  end
+  def create 
+  	puts 'Entrei no create'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get 'car/details'
   get 'home/index'
+  get 'car/cars'
+  post 'car/cars' => 'car#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
