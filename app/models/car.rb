53ab384ec375d8407ac3e455c743b4aa7b997ccb
Andre <andre.marques212747@hotmail.com>
2015-01-13 18:53:03 -0200

    Criação de validação campo valor convert string to decimal, configuração tradução validação e limitação de caracteres dos campos do cadastro de carro

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,9 +1,10 @@
 class Car < ActiveRecord::Base
-	validates :year, presence: true, length: {minimum:2}
-
+	
 	validates :brand, presence:true
 	
 	validates :model, presence: true
+
+	validates :year, presence: true
 
 	validates :km, presence: true
 
