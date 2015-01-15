8ebd43f970058efe167649cd8a07e18fc5d0fa16
Diego Alves <diego.oborba@gmail.com>
2015-01-15 20:55:21 -0200

    adicionando carros com destaque no carrosel

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,7 @@
 class Car < ActiveRecord::Base
-	scope :usados, -> { where ("km > 0") }
-	scope :novos,  -> { where ("km == 0 and value < 100000") }
-	scope :luxos,  -> { where ("km == 0 and value >= 100000") }
-	
+	scope :usados, 		-> { where ("km > 0") }
+	scope :novos,  		-> { where ("km == 0 and value < 100000") }
+	scope :luxos,  		-> { where ("km == 0 and value >= 100000") }
+	scope :destaques, -> { where ({featured: true}) }	
 	validates :brand, :model, :year, :km, :value, presence: true 
 end
