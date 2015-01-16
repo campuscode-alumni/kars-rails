d6db3904ea79512b74afc2fdd5be0a34b134c86a
Alan R. R. Batista <alanrrb@gmail.com>
2015-01-16 20:19:13 -0200

    specs para o model car

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,6 +2,11 @@ class Car < ActiveRecord::Base
 	scope :usados, 		-> { where ("km > 0") }
 	scope :novos,  		-> { where ("km == 0 and value < 100000") }
 	scope :luxos,  		-> { where ("km == 0 and value >= 100000") }
-	scope :destaques, -> { where ({featured: true}) }	
+	scope :destaques,   -> { where ({featured: true}) }	
+	
 	validates :brand, :model, :year, :km, :value, presence: true 
+
+	def usado?
+		km > 0
+	end
 end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -2,15 +2,46 @@ require 'rails_helper'
 
 describe Car, :type => :model do
   describe "#usados" do
-    it "returns only second hand cars"
+    it "returns only second hand cars" do
+      Car.create({:brand => "VW", "value" => "20000", "model" => "Gol", "year" => "2014", "km" => "1", "motor" => "2.0"})
+      Car.create({:brand => "VW", "value" => "20000", "model" => "Gol", "year" => "2014", "km" => "55430", "motor" => "2.0"})
+      Car.create({:brand => "VW", "value" => "20000", "model" => "Gol", "year" => "2014", "km" => "0", "motor" => "2.0"})
+
+      expect(Car.usados.length).to eq(2)
+    end
   end
   describe "#novos" do
-    it "returns only new cars"
+    it "returns only new cars" do
+      Car.create({:brand => "VW", "value" => "20000", "model" => "Gol", "year" => "2014", "km" => "1", "motor" => "2.0"})
+      Car.create({:brand => "VW", "value" => "20000", "model" => "Gol", "year" => "2014", "km" => "55430", "motor" => "2.0"})
+      Car.create({:brand => "VW", "value" => "20000", "model" => "Gol", "year" => "2014", "km" => "0", "motor" => "2.0"})
+
+      expect(Car.novos.length).to eq(1)
+    end
   end
   describe "#luxos" do
-    it "returns only luxury cars"
+    it "returns only luxury cars" do
+      Car.create({:brand => "VW", "value" => "20000", "model" => "Gol", "year" => "2014", "km" => "1", "motor" => "2.0"})
+      Car.create({:brand => "VW", "value" => "20000", "model" => "Gol", "year" => "2014", "km" => "55430", "motor" => "2.0"})
+      Car.create({:brand => "VW", "value" => "200000", "model" => "Gol", "year" => "2014", "km" => "0", "motor" => "2.0"})
+
+      expect(Car.luxos.length).to eq(1)
+    end
   end
   describe "validates" do
-    it "blank fields"
+    describe "blank fields" do
+      it "all fields are mandatory" do
+        car = Car.create
+        expect(car.errors.messages.length).to eq(5)
+      end
+      it "km is blank" do 
+        car = Car.create({:brand => "VW", "value" => "20000", "model" => "Gol", "year" => "2014", "motor" => "2.0"})
+        expect(car.errors.messages[:km][0]).to eq(": Favor preencher o campo.")
+      end
+      it "year is blank"
+      it "model is blank"
+      it "brand is blank"
+      it "value is blank"
+    end
   end
 end
