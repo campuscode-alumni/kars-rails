a59b7b99da5377c3342701a21d76c5b1ff4a65d0
Vitor Rocha <vitor.rocha81@bol.com.br>
2015-01-16 22:18:46 -0200

    Testes rspec

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -21,11 +21,9 @@ describe Car, :type => :model do
   end
   describe "#luxos" do
     it "returns only luxury cars" do
-      Car.create({:brand => "VW", "value" => "20000", "model" => "Gol", "year" => "2014", "km" => "1", "motor" => "2.0"})
-      Car.create({:brand => "VW", "value" => "20000", "model" => "Gol", "year" => "2014", "km" => "55430", "motor" => "2.0"})
-      Car.create({:brand => "VW", "value" => "200000", "model" => "Gol", "year" => "2014", "km" => "0", "motor" => "2.0"})
-
-      expect(Car.luxos.length).to eq(1)
+      Car.create({:brand => "VW", "value" => "ccfxxx", "model" => "Gol", "year" => "2014", "km" => "0", "motor" => "2.0"})
+      
+      expect(Car.luxos.length).to eq(0)
     end
   end
   describe "validates" do
@@ -34,12 +32,9 @@ describe Car, :type => :model do
         car = Car.create
         expect(car.errors.messages.length).to eq(5)
       end
-      it "km is blank" do 
-        car = Car.create({:brand => "VW", "value" => "20000", "model" => "Gol", "year" => "2014", "motor" => "2.0"})
-        expect(car.errors.messages[:km][0]).to eq(": Favor preencher o campo.")
+      it "model is blank" do
+
       end
-      it "year is blank"
-      it "model is blank"
       it "brand is blank"
       it "value is blank"
     end
