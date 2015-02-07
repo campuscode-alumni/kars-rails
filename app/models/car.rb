426ed233c418f81877f7df84224169f1a8c34883
Alan R. R. Batista <alanrrb@gmail.com>
2015-02-07 18:00:47 -0200

    fix for mysql

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,7 @@
 class Car < ActiveRecord::Base
 	scope :usados,    -> { where ("km > 0") }
-	scope :novos,     -> { where ("km == 0 and value < 100000") }
-	scope :luxos,     -> { where ("km == 0 and value >= 100000") }
+	scope :novos,     -> { where ("km = 0 and value < 100000") }
+	scope :luxos,     -> { where ("km = 0 and value >= 100000") }
 	scope :destaques, -> { where ({featured: true}) }	
 	
 	validates_presence_of :brand, :model, :year, :km, :value, :fuel
